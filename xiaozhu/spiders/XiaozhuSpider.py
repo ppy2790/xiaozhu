64121ae48a109cb1231b36351e4bd75282cad946
ppy2790 <ppy2790@126.com>
2017-02-02 10:53:28 +0800

    scrapy增加随机请求头user_agent

---
+++ xiaozhu/spiders/XiaozhuSpider.py
@@ -0,0 +1,58 @@
+#coding=utf-8
+
+from scrapy.spiders import CrawlSpider
+from scrapy.http import Request, FormRequest
+from scrapy.selector import Selector
+from xiaozhu.items import XiaozhuItem
+
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class XiaozhuSpider(CrawlSpider):
+
+    name = 'xiaozhu'
+
+    start_urls=[
+        'http://sh.xiaozhu.com/search-duanzufang-p1-0/'
+    ]
+
+    def parse(self, response):
+        selector = Selector(response)
+
+        item = XiaozhuItem()
+
+        infos = selector.xpath("//ul[@class='pic_list clearfix']/li")
+
+        for info in infos:
+
+            price = info.xpath('div[2]/span[1]/i/text()').extract()[0]
+
+            print price
+
+            intro = info.xpath('div[2]/div/a/span/text()').extract()[0]
+
+            print intro
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
